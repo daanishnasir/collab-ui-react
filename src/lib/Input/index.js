276245ee5bbdfd424f2e7a262b0710f19bb1b4b0
Daanish Nasir <daanishnasir@gmail.com>
2018-11-27 23:11:10 -0600

    wip(react-examples): create sections for examples

--- src/lib/Input/index.js
+++ src/lib/Input/index.js
@@ -561,24 +561,6 @@ export default function InputSecondary() {
 }
 **/
 
-/**
-* @component input
-* @section validation
-* @react
-import { Input } from '@collab-ui/react';
-
-export default function InputSecondary() {
-  return (
-    <Input
-      name='inputSecondaryLabel'
-      label='Input with Secondary Label'
-      htmlId='inputSecondaryLabel'
-      inputSize='small-5'
-      secondaryLabel='Secondary Label'
-    />
-  );
-}
-**/
 
 /**
 * @component input
