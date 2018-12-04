b331729e7226b5825b804fd03469af52061f14e9
Daanish Nasir <daanishnasir@gmail.com>
2018-12-04 12:19:39 -0600

    wip(react-examples): fix exported className and function examples, remove unnecessary styling

--- src/lib/Link/index.js
+++ src/lib/Link/index.js
@@ -81,11 +81,7 @@ import { Link } from '@collab-ui/react';
 
 export default function LinkColored() {
   return(
-    <div className='row' style={{marginBottom: '1rem'}}>
-      <div className='columns small-3'>
-        <Link tag='div' color='red'>Color Prop(red)</Link>
-      </div>
-    </div>
+    <Link tag='div' color='red'>Color Prop(red)</Link>
   );
 }
 **/
